09ab353b079d3b6e3725d1517d5c439bd9741944
Sebastina <sebane1@thavea.com>
2025-01-12 15:45:14 -0600

    Initial commit of bone reading logic. Based off Ktsis

---
+++ MediaBoneManager.cs
@@ -0,0 +1,75 @@
+﻿using Dalamud.Game.ClientState.Objects.Types;
+using Ktisis.Structs.Actor;
+using Ktisis.Structs;
+using RoleplayingMediaCore;
+using System.Collections.Generic;
+using System.Numerics;
+using System;
+using FFXIVClientStructs.FFXIV.Common.Lua;
+using RoleplayingVoice;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
+using RoleplayingVoiceDalamudWrapper;
+
+namespace RoleplayingVoiceDalamud.GameObjects {
+    public class MediaBoneManager {
+        public static Dictionary<string, Dictionary<string, MovingObject>> _lastBonePositions = new Dictionary<string, Dictionary<string, MovingObject>>();
+        public static void CheckForValidBoneSounds(ICharacter character, CharacterVoicePack characterVoicePack,
+            RoleplayingMediaManager roleplayingMediaManager, MediaManager mediaManager) {
+            unsafe {
+                if (!_lastBonePositions.ContainsKey(character.Name.TextValue)) {
+                    _lastBonePositions[character.Name.TextValue] = new Dictionary<string, MovingObject>();
+                }
+                try {
+                    if (character != null) {
+                        Actor* characterActor = (Actor*)character.Address;
+                        var model = characterActor->Model;
+                        if (model != null && model->Skeleton != null) {
+                            for (int i = 0; i < model->Skeleton->PartialSkeletonCount; i++) {
+                                var partialSkeleton = model->Skeleton->PartialSkeletons[i];
+                                var pos = partialSkeleton.GetHavokPose(0);
+                                if (pos != null) {
+                                    var skeleton = pos->Skeleton;
+                                    for (var i2 = 1; i2 < skeleton->Bones.Length; i2++) {
+                                        if (model->Skeleton != null) {
+                                            var bone = model->Skeleton->GetBone(i, i2);
+                                            if (bone.HkaBone.Name.String != null) {
+                                                if (!_lastBonePositions[character.Name.TextValue].ContainsKey(bone.HkaBone.Name.String)) {
+                                                    _lastBonePositions[character.Name.TextValue][bone.HkaBone.Name.String] = new MovingObject(new Vector3(), new Vector3(), false);
+                                                }
+                                                var movingObject = _lastBonePositions[character.Name.TextValue][bone.HkaBone.Name.String];
+
+                                                var worldPos = bone.GetWorldPos(characterActor, model);
+                                                var rotation = MediaBoneObject.Q2E(bone.Transform.Rotation);
+                                                float distance = Vector3.Distance(movingObject.LastPosition, worldPos);
+                                                float rotationDistance = Vector3.Distance(movingObject.LastRotation, rotation);
+                                                if (distance > 2f || rotationDistance > 2f) {
+                                                    if (!movingObject.IsMoving) {
+                                                        string value = characterVoicePack.GetMisc(bone.HkaBone.Name.String, false, true);
+                                                        if (!string.IsNullOrEmpty(value)) {
+                                                            var boneObject = new MediaBoneObject(bone, characterActor, model);
+                                                            string boneName = bone.HkaBone.Name.String;
+                                                            Plugin.PluginLog.Verbose(boneName + " playing sound.");
+                                                            mediaManager.PlayMedia(boneObject, value, SoundType.LoopWhileMoving, false, 0, default, (object o, string args) => {
+                                                                movingObject.IsMoving = false;
+                                                                Plugin.PluginLog.Verbose(boneName + " stopping sound.");
+                                                            });
+                                                            movingObject.IsMoving = true;
+                                                        }
+                                                    }
+                                                }
+                                                movingObject.LastPosition = worldPos;
+                                                movingObject.LastRotation = rotation;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch {
+
+                }
+            }
+        }
+    }
+}
---
+++ MediaBoneObject.cs
@@ -0,0 +1,76 @@
+﻿using FFXIVClientStructs.Havok.Common.Base.Math.Quaternion;
+using Ktisis.Structs.Actor;
+using Ktisis.Structs.Bones;
+using RoleplayingMediaCore;
+using System;
+using System.Numerics;
+
+namespace RoleplayingVoiceDalamud.GameObjects {
+    public unsafe class MediaBoneObject : IMediaGameObject {
+        private Bone _bone;
+        private unsafe Actor* _actor;
+        private unsafe ActorModel* _actorModel;
+        string _name = "";
+        private bool _invalid;
+
+        public unsafe MediaBoneObject(Bone bone, Actor* actor, ActorModel* actorModel) {
+            _bone = bone;
+            _actor = actor;
+            _actorModel = actorModel;
+            _name = _bone.HkaBone.Name.String + _actor->GetName();
+        }
+
+        string IMediaGameObject.Name => _name;
+
+        Vector3 IMediaGameObject.Position {
+            get {
+                try {
+                    if (_actor != null && _actorModel != null && _bone != null) {
+                        return _bone.GetWorldPos(_actor, _actorModel);
+                    } else {
+                        return Vector3.Zero;
+                    }
+                } catch {
+                    _invalid = true;
+                    return Vector3.Zero;
+                }
+            }
+        }
+        Vector3 IMediaGameObject.Rotation {
+            get {
+                try {
+                    if (_bone != null) {
+                        return Q2E(_bone.Transform.Rotation);
+                    } else {
+                        return Vector3.Zero;
+                    }
+                } catch {
+                    _invalid = true;
+                    return Vector3.Zero;
+                }
+            }
+        }
+
+        Vector3 IMediaGameObject.Forward => new Vector3();
+
+        Vector3 IMediaGameObject.Top => new Vector3();
+        public static Vector3 Q2E(hkQuaternionf q) // Returns the XYZ in ZXY
+    {
+            Vector3 angles;
+
+            angles.X = (float)Math.Atan2(2 * (q.W * q.X + q.Y * q.Z), 1 - 2 * (q.X * q.X + q.Y * q.Y));
+            if (Math.Abs(2 * (q.W * q.Y - q.Z * q.X)) >= 1) angles.Y = (float)Math.CopySign(Math.PI / 2, 2 * (q.W * q.Y - q.Z * q.X));
+            else angles.Y = (float)Math.Asin(2 * (q.W * q.Y - q.Z * q.X));
+            angles.Z = (float)Math.Atan2(2 * (q.W * q.Z + q.X * q.Y), 1 - 2 * (q.Y * q.Y + q.Z * q.Z));
+
+            return new Vector3() {
+                X = (float)(180 / Math.PI) * angles.X,
+                Y = (float)(180 / Math.PI) * angles.Y,
+                Z = (float)(180 / Math.PI) * angles.Z
+            };
+        }
+        string IMediaGameObject.FocusedPlayerObject => "";
+
+        bool IMediaGameObject.Invalid => _invalid;
+    }
+}
