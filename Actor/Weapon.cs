c78c8d56d509c837e58263d3e2a8f4311e6c9b25
Sebastina <sebane1@thavea.com>
2025-04-01 18:23:33 -0500

    Update offsets for 7.2

--- Actor/Weapon.cs
+++ Actor/Weapon.cs
@@ -9,12 +9,12 @@ using FFXIVClientStructs.Havok.Common.Base.Math.QsTransform;
 namespace Ktisis.Structs.Actor {
 	[StructLayout(LayoutKind.Explicit)]
 	public struct Weapon {
-		[FieldOffset(0x00)] public WeaponEquip Equip;
-		[FieldOffset(0x08)] public unsafe WeaponModel* Model;
-		[FieldOffset(0x40)] public bool IsSheathed;
-		[FieldOffset(0x60)] public WeaponFlags Flags;
+        [FieldOffset(0x00)] public WeaponEquip Equip;
+        [FieldOffset(0x18)] public unsafe WeaponModel* Model;
+        [FieldOffset(0x40)] public bool IsSheathed;
+        [FieldOffset(0x60)] public WeaponFlags Flags;
 
-		public unsafe WeaponEquip GetEquip()
+        public unsafe WeaponEquip GetEquip()
 			=> this.Model != null ? this.Model->Equip : this.Equip;
 
 		public unsafe void SetEquip(WeaponEquip item) {
@@ -25,25 +25,28 @@ namespace Ktisis.Structs.Actor {
 
 	[StructLayout(LayoutKind.Explicit)]
 	public struct WeaponEquip {
-		[FieldOffset(0x00)] public ushort Set;
-		[FieldOffset(0x02)] public ushort Base;
-		[FieldOffset(0x04)] public ushort Variant;
-		[FieldOffset(0x06)] public ushort Dye;
-	}
+        [FieldOffset(0x00)] public ushort Set;
+        [FieldOffset(0x02)] public ushort Base;
+        [FieldOffset(0x04)] public ushort Variant;
+        [FieldOffset(0x06)] public byte Dye;
+        [FieldOffset(0x07)] public byte Dye2;
+
+        [FieldOffset(0x00)] public ulong Value;
+    }
 
 	[StructLayout(LayoutKind.Explicit)]
 	public struct WeaponModel {
-		[FieldOffset(0x50)] public hkQsTransformf Transform;
-		[FieldOffset(0x50)] public Vector3 Position;
-		[FieldOffset(0x60)] public Quaternion Rotation;
-		[FieldOffset(0x70)] public Vector3 Scale;
-		
-		[FieldOffset(0x88)] public byte Flags;
+        [FieldOffset(0x50)] public hkQsTransformf Transform;
+        [FieldOffset(0x50)] public Vector3 Position;
+        [FieldOffset(0x60)] public Quaternion Rotation;
+        [FieldOffset(0x70)] public Vector3 Scale;
 
-		[FieldOffset(0xA0)] public unsafe Skeleton* Skeleton;
+        [FieldOffset(0x88)] public byte Flags;
 
-		[FieldOffset(0x8F0)] public WeaponEquip Equip;
-	}
+        [FieldOffset(0xA0)] public unsafe Skeleton* Skeleton;
+
+        [FieldOffset(0xA20)] public WeaponEquip Equip;
+    }
 
 	public enum WeaponSlot {
 		MainHand = 0,
