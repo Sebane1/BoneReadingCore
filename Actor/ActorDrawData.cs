989264f9db944056906c8835f7b4dac4626d2c46
Sebastina <sebane1@thavea.com>
2025-08-13 17:34:58 -0500

    Update signatures for patch 7.3

--- Actor/ActorDrawData.cs
+++ Actor/ActorDrawData.cs
@@ -4,10 +4,11 @@ namespace Ktisis.Structs.Actor {
 	[StructLayout(LayoutKind.Explicit)]
 	public struct ActorDrawData {
 		[FieldOffset(0x010)] public Weapon MainHand;
-		[FieldOffset(0x080)] public Weapon OffHand;
-		[FieldOffset(0x0E8)] public Weapon Prop;
-		
-		[FieldOffset(0x160)] public Equipment Equipment;
-		[FieldOffset(0x188)] public Customize Customize;
-	}
+        [FieldOffset(0x080)] public Weapon OffHand;
+        [FieldOffset(0x0F0)] public Weapon Prop;
+
+        [FieldOffset(0x1D0)] public Equipment Equipment;
+        [FieldOffset(0x220)] public Customize Customize;
+        [FieldOffset(0x240)] public ushort Glasses;
+    }
 }
